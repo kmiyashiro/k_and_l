6efcb321fafabb96cde4c4557db47c376c63fdb9
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2019-11-21 10:30:40 -0800

    Working

--- scraper.js
+++ scraper.js
@@ -5,55 +5,88 @@ var request = require("request");
 var sqlite3 = require("sqlite3").verbose();
 
 function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+  // Set up sqlite database.
+  var db = new sqlite3.Database("data.sqlite");
+  db.serialize(function() {
+    db.run(
+      "CREATE TABLE IF NOT EXISTS data (key PRIMARY KEY, id TEXT, date TEXT, name TEXT, price INT)"
+    );
+    callback(db);
+  });
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+function updateRow(db, id, name, price) {
+  let date = new Date();
+  let dateString =
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+  // Insert some data.
+  var statement = db.prepare(`INSERT INTO data(key, id, date, name, price)
+    VALUES (?, ?, ?, ?, ?)
+    ON CONFLICT(key) DO UPDATE SET
+      price=excluded.price
+  `);
+  statement.run(`${dateString}-${id}`, id, dateString, name, price);
+  statement.finalize();
 }
 
 function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+  // Read some data.
+  db.each("SELECT name, date, price FROM data", function(err, row) {
+    console.log(`${row.date} ${row.name}: ${row.price}`);
+  });
+}
+
+function getSourceId(url) {
+  return url.match(/i=([\d]+?)\&/)[1];
 }
 
 function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
+  // Use request to read in pages.
+  request(url, function(error, response, body) {
+    if (error) {
+      console.log("Error requesting page: " + error);
+      return;
+    }
 
-		callback(body);
-	});
+    callback(body);
+  });
 }
 
 function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
+  // Use request to read in pages.
+  fetchPage(
+    "https://www.klwines.com/Products?&filters=sv2_206!20&limit=500&offset=0",
+    function(body) {
+      // Use cheerio to find things in the page with css selectors.
+      var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
+      var elements = $(".result");
+      // console.log("Elements", elements);
+      elements.each(function() {
+        var link = $(this)
+          .find(".result-desc > a[href^='/p/i']")
+          .first();
 
-		readRows(db);
+        var id = getSourceId(link.attr("href"));
+        var name = link.text().trim();
+        var price = parseInt(
+          $(this)
+            .find(".price strong")
+            .text()
+            .trim()
+            .replace(/[\$\.]/g, "")
+        );
+        console.log("id", id);
+        console.log("name", name);
+        console.log("price", price);
 
-		db.close();
-	});
+        updateRow(db, id, name, price);
+      });
+
+      db.close();
+    }
+  );
 }
 
 initDatabase(run);
