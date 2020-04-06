73a6d45181435a887810189ac18f7ada7de8ef5c
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2020-04-06 16:19:30 -0700

    Scrape all spirits

--- scraper.js
+++ scraper.js
@@ -52,27 +52,27 @@ function initDatabase(callback) {
   const db = new sqlite3.Database('data.sqlite')
   db.serialize(function () {
     db.run(
-      'CREATE TABLE IF NOT EXISTS data (key PRIMARY KEY, id TEXT, date TEXT, name TEXT, price INT)'
+      'CREATE TABLE IF NOT EXISTS data (key PRIMARY KEY, id TEXT, category TEXT, date TEXT, name TEXT, price INT)'
     )
     callback(db)
   })
 }
 
-function updateRow(db, id, name, price) {
-  console.log('UPDATE', id, name, price)
+function updateRow(db, id, category, name, price) {
+  console.log('UPDATE', id, category, name, price)
   let date = new Date()
   let dateString =
     date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
 
   // Insert some data.
-  const statement = db.prepare(`INSERT INTO data(key, id, date, name, price)
-    VALUES (?, ?, ?, ?, ?)
+  const statement = db.prepare(`INSERT INTO data(key, id, category, date, name, price)
+    VALUES (?, ?, ?, ?, ?, ?)
     ON CONFLICT(key) DO UPDATE SET
       price=excluded.price
   `)
-  statement.run(`${dateString}-${id}`, id, dateString, name, price)
+  statement.run(`${dateString}-${id}`, id, category, dateString, name, price)
   statement.finalize()
-  console.log('FINALIZE', id, name, price)
+  console.log('FINALIZE', id, category, name, price)
 }
 
 function readRows(db) {
@@ -86,24 +86,43 @@ function getSourceId(url) {
   return url.match(/i=([\d]+?)\&/)[1]
 }
 
-async function run(db) {
-  console.log('running')
-  console.log('getting cookie')
-  await getCookie()
-  // Use request to read in pages.
-  console.log('fetching page')
-  const response = await axios(
-    'https://www.klwines.com/Products?&filters=sv2_206!20&limit=500&offset=0'
-  )
-  // Use cheerio to find things in the page with css selectors.
+async function getCategories() {
+  const CATEGORY_SELECTOR = '#category-menu-container-Variety .category-link a'
+
+  const response = await axios('https://www.klwines.com/Spirits')
+  const $ = cheerio.load(response.data)
+  const categoryLinks = $(CATEGORY_SELECTOR)
+  if (categoryLinks.length === 0) {
+    throw new Error('Could not find category links')
+  }
+
+  const hrefs = []
+
+  categoryLinks.each(function (_, link) {
+    hrefs.push($(this).attr('href'))
+  })
+
+  return hrefs.map((href) => {
+    const match = href.match(/\!(\d+)[^\d]/)
+    if (!match) {
+      console.log('missing match', href)
+    } else {
+      return match[1]
+    }
+  })
+}
+
+async function scrapeCategory(category, db) {
+  console.log('CATEGORY', category)
+  const url = `https://www.klwines.com/Products?&filters=sv2_206!${category}&limit=500&offset=0`
+  const response = await axios(url)
   const $ = cheerio.load(response.data)
 
   const elements = $('.tf-product')
   console.log(`Found ${elements.length} elements`)
   if (elements.length === 0) {
-    throw new Error(
-      'Did not find any results, probably need to change selector'
-    )
+    console.log(`Did not find any results for ${url}`)
+    return
   }
   // console.log("Elements", elements);
   elements.each(function () {
@@ -120,8 +139,30 @@ async function run(db) {
     )
     console.log('id:', id, 'name:', name, 'price:', price)
 
-    updateRow(db, id, name, price)
+    updateRow(db, id, category, name, price)
   })
+  return elements.length
+}
+
+async function run(db) {
+  console.log('running')
+  console.log('getting cookie')
+  await getCookie()
+  // Use request to read in pages.
+  console.log('fetching categories')
+  const categories = await getCategories()
+  console.log('found categories:', categories.length)
+
+  let updatedCount = 0
+
+  while (categories.length) {
+    updatedCount += await scrapeCategory(categories.pop(), db)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
+  if (updatedCount === 0) {
+    throw new Error('Nothing found, possibly broken selectors')
+  }
 
   db.close()
 }
